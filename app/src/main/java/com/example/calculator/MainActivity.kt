400a01236731fb66907640ce7596a0042eb57036
Abdul Moiz Khan <136478539+Abdul-Moiz-Khan1@users.noreply.github.com>
2024-04-24 20:14:20 +0500

    Calculator

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,86 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Editable
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import com.example.calculator.databinding.ActivityMainBinding
+import net.objecthunter.exp4j.Expression
+import net.objecthunter.exp4j.ExpressionBuilder
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var expression: Expression
+    private var decimal = true
+    private var operator = true
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    fun ClearAll(view: View) {
+        binding.result.text = ""
+        binding.calculation.text = ""
+    }
+
+
+    fun Backspace(view: View) {
+        binding.calculation.text = binding.calculation.text.toString().dropLast(1)
+        try {
+            val lastchar = binding.calculation.text.toString().last()
+            if (lastchar.isDigit()) {
+                onEqual()
+            }
+        }catch (ex : Exception){
+            Log.e("Last Char Error" , ex.toString())
+        }
+    }
+
+
+    fun NumberAction(view: View) {
+        if (view is Button) {
+            if (view.text == ".") {
+                if (decimal) {
+                    binding.calculation.append(view.text)
+                    decimal = false
+                }
+            } else
+                binding.calculation.append(view.text)
+            operator = true
+        }
+        onEqual()
+    }
+
+    fun OperatorAction(view: View) {
+        if (view is Button && operator) {
+            binding.calculation.append(view.text)
+            operator = false
+            decimal = true
+        }
+    }
+
+    fun Equal(view: View) {
+//        onEqual()
+    }
+
+    fun onEqual(){
+        if(decimal && operator){
+            val txt = binding.calculation.text.toString()
+            expression = ExpressionBuilder(txt).build()
+            try{
+                val resultfin = expression.evaluate()
+                binding.result.text = resultfin.toString()
+            }catch (ex: ArithmeticException){
+                Log.e("Evaluation Error" , ex.toString())
+                binding.result.text = "Error"
+
+            }
+        }
+    }
+
+}
